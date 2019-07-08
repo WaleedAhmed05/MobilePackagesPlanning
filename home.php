4f693ca51be1e84b02d601d849d8354d802dc5a3
WaleedAhmed05 <52674043+WaleedAhmed05@users.noreply.github.com>
2019-07-08 23:12:29 +0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+
+
+<?php
+
+
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "mobilepackage";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+
+
+?>
+
+
+<html>
+<head>
+<title>Mobile Package Planning</title>
+</head>
+<body>
+<font color="white">
+<style>
+body {
+background-image: url('Frame_Background.jpg') }
+</style>
+<br>
+<style>
+a:link {
+    color: white;
+    background-color: transparent;
+    text-decoration: none;
+}
+a:visited {
+    color: white;
+    background-color: transparent;
+    text-decoration: none;
+}
+h2{display: inline;}
+</style>
+<center>
+<a href="home.php"><img src="images/MainLogo.png">
+</a>
+<br>
+<h2>Mobile Package Planning </h2>
+<br>
+<h2>Under Construction... </h2>
+</center>
+
+<B><center><h1><a href="/home.php">Home </a>| Downloads <font size="-3">Coming soon </font>  | About Us  <font size="-3">Coming soon </font>  | Contact Us <font size="-3">Coming soon </font> </h1> </center></B>
+<marquee><h2>Mobile Package Planning | Compare and get the most Suitable Pakckage</h2></marquee>
+<br><br><br><br><br>
+
+<center>
+<pre><a href="sms.php"><img src="SMS_CON3.png"><h2>SMS</h2>                       </a><a href="calls.php"><img src="CALL_CON3.png"><h2>Calls</h2><br><br><br><br><br>
+
+</a><a href="internet.php"><img src="INT_CON3.png"><h2>Internet</h2>                 </a><a href="combo.php"><img src="COMBO_CON3.png"><h2>COMBO</h2>
+</a></pre><a href="dailycombo.php">
+</a></center>
+
+
+<br><br><br><br><br><br>
+<center><h3>Mobile Package Planning | 2017</h3></center>
+</font>
+</body>
+</html>
+
